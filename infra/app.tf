0f8c42f894868fd5bff924d7e5fe73e9635631cd
478859-anurag <58522874+478859-anurag@users.noreply.github.com>
2022-04-30 13:54:05 +0100

    updatingVPCID

--- infra/app.tf
+++ infra/app.tf
@@ -2,6 +2,10 @@ resource "aws_ecs_cluster" "app" {
   name = "${var.prefix}-app"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
 resource "aws_ecs_task_definition" "app" {
   family                   = var.prefix
   requires_compatibilities = ["FARGATE"]
@@ -85,7 +89,7 @@ resource "aws_lb_target_group" "app" {
   port        = 3000
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = var.vpc_id
+  vpc_id      = aws_vpc.main.id
 
   health_check {
     path = "/healthcheck"
@@ -120,7 +124,7 @@ resource "aws_lb_listener" "app_443" {
 resource "aws_security_group" "lb_sg" {
   name        = "${var.prefix}-lb-sg"
   description = "Allow connections from internet"
-  vpc_id      = var.vpc_id
+  vpc_id      = aws_vpc.main.id
 
   ingress = [
     {
@@ -169,7 +173,7 @@ resource "aws_security_group" "lb_sg" {
 resource "aws_security_group" "app" {
   name        = "${var.prefix}-server"
   description = "Allow connections from the load balancer"
-  vpc_id      = var.vpc_id
+  vpc_id      = aws_vpc.main.id
 
   ingress = [
     {
